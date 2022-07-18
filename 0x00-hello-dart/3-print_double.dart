4905d4b52980f86af4f8703f3a30d52f70a02fe9
jyuly12 <yuly.gonzalez2021@gmail.com>
2022-07-18 16:07:01 -0500

    Add files

---
+++ 0x00-hello-dart/3-print_double.dart
@@ -0,0 +1,5 @@
+void main() {
+  var n = 3.14159265359;
+  String double = n.toStringAsFixed(2);
+  print('Double: ${double}\n');
+}
---
+++ 0x00-hello-dart/4-print_string.dart
@@ -0,0 +1,5 @@
+void main() {
+  String str = "Holberton School";
+  String substr = str.substring(0,9);
+  print('${str * 3}\n${substr}\n');
+}
