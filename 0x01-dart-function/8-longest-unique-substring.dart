8044eafa1e8ed2a58667f884bc45d70bf1938bca
jyuly12 <yuly.gonzalez2021@gmail.com>
2022-07-20 22:30:26 -0500

    fix file

--- 0x01-dart-function/8-longest-unique-substring.dart
+++ 0x01-dart-function/8-longest-unique-substring.dart
@@ -2,10 +2,11 @@ String longestUniqueSubstring(String str){
 
   String letters = "";
   for(var i = 0; i < str.length; i++){
-     if(letters.contains(str[i])){
-       continue;
-     }else
-       letters += str[i];
+    if(letters.contains(str[i])){
+      letters = letters.substring(letters.indexOf(str[i]) + 1);
+     }
+    letters += str[i];
   }
   return(letters);
 }
+  
